9afee06d2b6e774b2aa75bc8520b071a20a766be
NoNonsense126 <retail_nns@hotmail.com>
2014-11-27 12:00:25 +0800

    api up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,16 @@
 module Api
   class UsersController < Api::BaseController
 
+  	def create
+      @user = (User.new(user_params))
+
+      if @user.save
+        render :show, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+  	end
+
     private
 
       def user_params
