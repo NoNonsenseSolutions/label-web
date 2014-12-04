abdca4894e094db2911bc4b67688244264f3a6f7
NoNonsense126 <retail_nns@hotmail.com>
2014-12-04 16:39:43 +0800

    file url change

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -25,8 +25,8 @@ class Photo < ActiveRecord::Base
 
   private
   	def validate_minimum_image_size
-  		if file.url
-	  		image = MiniMagick::Image.open(file.url)
+  		if file.path
+	  		image = MiniMagick::Image.open(file.path)
 	  		unless image[:width] > 400
 	  			errors.add :image, "should be 400px wide minimum!"
 	  		end
